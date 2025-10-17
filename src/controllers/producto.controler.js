e5092b5b6a7c0f5b1f4d777f96cba7d04c882c9d
Maxi Ordoñez <exemaxi@outlook.com>
2025-10-17 20:34:15 -0300

    Se implemento la logica para crear producto.

--- src/controllers/producto.controler.js
+++ src/controllers/producto.controler.js
@@ -1,10 +1,21 @@
-//Archivo solo para rutas del Crud
+import Producto from "../models/producto.js";
 
+//Archivo solo para rutas del Crud
 export const prueba = (req, res)=>{
 console.log("Desde el controlador de puebas")
 res.send('Desde el controlador de puebas')
 }
 
-export const crearProducto = (req, res)=>{
-res.send('Aqui se tiene que crear el producto')
+export const crearProducto = async (req, res)=>{
+try {
+// verificar que llegen los datos validados.
+//pedir al modelo producto, crear el objeto en la base de datos.
+console.log(req)
+const nuevoProducto = new Producto(req.body)
+await nuevoProducto.save()
+res.status(201).json({mensaje: "Producto creado correctamente"})
+} catch (error) {
+    console.log(error)
+    res.status(500).json({mensaje: "Error al crear el producto" })
 }
+};
