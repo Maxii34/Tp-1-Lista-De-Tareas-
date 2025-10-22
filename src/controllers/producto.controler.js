4a8df1c605dbe962ce5ba87b2e45051644d55c56
Maxi Ordoñez <exemaxi@outlook.com>
2025-10-22 15:06:36 -0300

    Se agrego el controlardor para borrar tareas por id.

--- src/controllers/producto.controler.js
+++ src/controllers/producto.controler.js
@@ -5,7 +5,7 @@ import tarea from "../models/producto.js";
 // Crear tarea
 export const crearTarea = async (req, res) => {
   try {
-    const nuevaTarea = new tarea(req.body); 
+    const nuevaTarea = new tarea(req.body);
     await nuevaTarea.save();
     res.status(201).json({ mensaje: "Tarea creada correctamente" });
   } catch (error) {
@@ -17,10 +17,39 @@ export const crearTarea = async (req, res) => {
 // Listar tareas
 export const listarTareas = async (req, res) => {
   try {
-    const tareas = await tarea.find(); 
+    const tareas = await tarea.find();
     res.status(200).json(tareas);
   } catch (error) {
     console.error(error);
     res.status(500).json({ mensaje: "Error al listar las tareas" });
+  }
+};
+// Obtener tarea por ID
+export const obtenerTareas = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const tareaEncontrada = await tarea.findById(req.params.id);
+    if (!tareaEncontrada) {
+      return res.status(404).json({ mensaje: "Tarea no encontrada" });
+    }
+    res.status(200).json(tareaEncontrada);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al obtener la tarea." });
+  }
+};
+//borrar tarea por id.
+export const borrarTareas = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const tareaEncontrada = await tarea.findById(req.params.id);
+    if (!tareaEncontrada) {
+      return res.status(404).json({ mensaje: "La tarea no encontrada" });
+    }
+    await tarea.findByIdAndDelete(req.params.id)
+    res.status(200).json({ mensaje: "La tarea eliminada correctamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al eliminar la tarea." });
   }
 };
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { crearTarea, listarTareas } from "../controllers/producto.controler.js";
+import { borrarTareas, crearTarea, listarTareas, obtenerTareas } from "../controllers/producto.controler.js";
 /*  GET, POST, PATH, PUT, DELETE. */
 
 const router = Router();
 
 router.route('/').post(crearTarea).get(listarTareas)
+router.route('/:id').get(obtenerTareas).delete(borrarTareas)
 
 export default router;
