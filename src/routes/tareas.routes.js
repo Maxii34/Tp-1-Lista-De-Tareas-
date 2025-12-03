5c7db9bcb5671c1a600f36cdcb7b5d74ae8a3175
Maxi Ordoñez <exemaxi@outlook.com>
2025-12-03 12:07:11 -0300

    Se agregaron las peticiones en las rutas, y se le implemento el validarToken a las peticiones censibles.

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,17 +1,25 @@
 import { Router } from "express";
-import { borrarTareas, crearTarea, editarTarea, listarTareas, obtenerTareas } from "../controllers/tareas.controler.js";
 import validarTareas from "../middlewares/validacionTareas.js";
 import validarIDTareas from "../middlewares/validacionIDTareas.js";
-
+import validarToken from "../middlewares/validarToken.js";
+import {
+  borrarTareas,
+  crearTarea,
+  editarTarea,
+  listarTareas,
+  obtenerTareas,
+} from "../controllers/tareas.controler.js";
 
 const router = Router();
 
-router.route('/')
-.post(validarTareas, crearTarea)
-.get(listarTareas)
-router.route('/:id')
-.get(validarIDTareas, obtenerTareas)
-.put([validarIDTareas ,validarTareas], editarTarea)
-.delete(validarIDTareas, borrarTareas)
+router
+  .route("/")
+  .post([validarToken, validarTareas], crearTarea)
+  .get(listarTareas);
+router
+  .route("/:id")
+  .get(validarIDTareas, obtenerTareas)
+  .put([validarToken, validarIDTareas, validarTareas], editarTarea)
+  .delete([validarToken, validarIDTareas], borrarTareas);
 
 export default router;
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
-import {  crearUsuario, listarUsuarios } from "../controllers/usuarios.controler.js";
+import {
+  crearUsuario,
+  listarUsuarios,
+  iniciarSesion,
+  obtenerUsuario,
+  borrarUsuario,
+} from "../controllers/usuarios.controler.js";
+import validarToken from "../middlewares/validarToken.js";
 
 const router = Router();
 
-router.route('/').post(crearUsuario).get(listarUsuarios)
+router.route("/").post(crearUsuario).get(validarToken, listarUsuarios);
+router.route("/login").post(iniciarSesion);
+
+// Rutas con ID
+router
+  .route("/:id")
+  .get(validarToken, obtenerUsuario)
+  .delete(validarToken, borrarUsuario);
 
 export default router;
