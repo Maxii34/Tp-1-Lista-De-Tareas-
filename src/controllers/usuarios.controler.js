4339127293fcf9223632faa54e1979deff0e309a
Maxi Ordoñez <exemaxi@outlook.com>
2025-12-03 12:25:03 -0300

    Se instalaron las dependencias de  bcryptjs y jsonwebtoken.

--- src/controllers/usuarios.controler.js
+++ src/controllers/usuarios.controler.js
@@ -1,5 +1,5 @@
 import usuarios from "../models/usuarios.js";
-import generarjwt from "./generarJWT.js";
+import generarjwt from "../middlewares/generarJWT.js";
 import bcrypt from "bcryptjs";
 
 export const crearUsuario = async (req, res) => {
